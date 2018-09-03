b5484fbfcd75d947467c42112fb28339a2704325
chendf2013 <chendf2013@163.com>
2018-09-03 23:26:50 +0800

    抽取app的创建逻辑到业务逻辑模块里面

---
+++ info/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_wtf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask_session import Session
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# 初始化数据库
+# SQLAchemy 是关系型数据库框架,需要导入flask-mysql和 flask-sqlalchemy
+db = SQLAlchemy(app)
+
+# 初始化 redis 对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 开启当前项目csrf保护
+CSRFProtect(app)
+
+# 设置seddion指定保存位置
+Session(app)
--- manage.py
+++ manage.py
@@ -1,28 +1,14 @@
 from flask_script import Manager
-from flask import Flask, session
-from flask_wtf import CSRFProtect
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_session import Session
-from config import Config
+from flask import  session
+from flask_migrate import Migrate, MigrateCommand
+from info import app, db
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# 初始化数据库
-# SQLAchemy 是关系型数据库框架,需要导入flask-mysql和 flask-sqlalchemy
-db = SQLAlchemy(app)
-
-# 初始化 redis 对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
-# 开启当前项目csrf保护
-CSRFProtect(app)
-
-# 设置seddion指定保存位置
-Session(app)
 manager = Manager(app)
+# 将app与db关联
+Migrate(app, db)
+# 将迁移命令添加到manager中
+manager.add_command("db", MigrateCommand)
 
 
 @app.route("/")
