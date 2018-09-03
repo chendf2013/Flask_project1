d97c054d96f65be4ab70ca7ea9ed174a4d1acba3
chendf2013 <chendf2013@163.com>
2018-09-03 23:17:07 +0800

    抽取配置到单独的文件

---
+++ config.py
@@ -0,0 +1,32 @@
+import base64
+import os
+
+from redis import StrictRedis
+
+
+class Config(object):
+    """配置信息"""
+    DEBUG = True
+
+    # 数据库的配置
+    # 配置数据库连接地址
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:mysql@127.0.0.1:3306/git"
+    # 是否追踪数据库的修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # redis 的配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+    # 指定 session 保存的redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 指定secrect_key
+    SECRET_KEY = base64.b64encode(os.urandom(48))
--- manage.py
+++ manage.py
@@ -1,41 +1,10 @@
-import os
 from flask_script import Manager
 from flask import Flask, session
 from flask_wtf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
-# 指定session的保存位置
 from flask_session import Session
-import base64
-
-
-
-class Config(object):
-    """配置信息"""
-    DEBUG = True
-
-    # 数据库的配置
-    # 配置数据库连接地址
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:mysql@127.0.0.1:3306/git"
-    # 是否追踪数据库的修改
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    # redis 的配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # session保存配置
-    SESSION_TYPE = "redis"
-    # 开启session签名
-    SESSION_USE_SIGNER = True
-    # 设置需要过期
-    SESSION_PERMANENT = False
-    # 设置过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-    # 指定 session 保存的redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 指定secrect_key
-    SECRET_KEY = base64.b64encode(os.urandom(48))
+from config import Config
 
 
 app = Flask(__name__)
@@ -53,14 +22,13 @@ CSRFProtect(app)
 
 # 设置seddion指定保存位置
 Session(app)
-
 manager = Manager(app)
 
 
 @app.route("/")
 def index():
     session["name"] = "chendf"
-    return "index"
+    return "5555"
 
 
 if __name__ == "__main__":
