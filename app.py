3d7f1bedc066fcb2bcf1f589b97597a737370f4b
chendf2013 <chendf2013@163.com>
2018-09-03 22:53:11 +0800

    session的位置配置

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+import os
+
+from flask import Flask,session
 from flask_wtf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 # 指定session的保存位置
-from flask_session import sessions
+from flask_session import Session
+import base64
+
 
 class Config(object):
     """配置信息"""
@@ -19,6 +23,19 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    # session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+    # 指定 session 保存的redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 指定secrect_key
+    SECRET_KEY = base64.b64encode(os.urandom(48))
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -33,9 +50,13 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启当前项目csrf保护
 CSRFProtect(app)
 
+# 设置seddion指定保存位置
+Session(app)
+
 
 @app.route("/")
 def index():
+    session["name"] = "chendf"
     return "index"
 
 
