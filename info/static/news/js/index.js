b6e809cb32b2bc47302f2eb8ff5ed52484188689
chendf2013 <chendf2013@163.com>
2018-09-08 14:46:54 +0800

    下拉加载

--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,4 +1,3 @@
-
 var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
@@ -29,37 +28,43 @@ $(function () {
 
     //页面滚动加载相关
     $(window).scroll(function () {
-
-        // 浏览器窗口高度
-        var showHeight = $(window).height();
-
-        // 整个网页的高度
-        var pageHeight = $(document).height();
-
-        // 页面可以滚动的距离
-        var canScrollHeight = pageHeight - showHeight;
-
-        // 页面滚动了多少,这个是随着页面滚动实时变化的
-        var nowScroll = $(document).scrollTop();
-
-        if ((canScrollHeight - nowScroll) < 100) {
-            // TODO 判断页数，去更新新闻数据
+    // 浏览器窗口高度
+    var showHeight = $(window).height();
+    // 整个网页的高度
+    var pageHeight = $(document).height();
+    // 页面可以滚动的距离
+    var canScrollHeight = pageHeight - showHeight;
+    // 页面滚动了多少,这个是随着页面滚动实时变化的
+    var nowScroll = $(document).scrollTop();
+    if ((canScrollHeight - nowScroll) < 100) {
+        // 判断页数，取更新数据
+      // 没有加载数据，取加载数据
+        if (!data_querying){
+            data_querying=true
+            // 去加载数据,先判断是否有数据可以加载
+            if (cur_page<total_page){
+                cur_page += 1;
+                updateNewsData()
+            }
         }
-    })
+    }
+})
 })
 
 function updateNewsData() {
-    // TODO 更新新闻数据
+    // 更新新闻数据
    var params = {
-        "page": 1,
+        "page": cur_page,
         "cid": currentCid,
         'per_page': 50
     };
         $.get("/news_list", params, function (resp) {
-        if (resp.errno=="0") {
-            alert("haha");
-            // 先清空原有数据
-            $(".list_con").html('')
+            // 数据加载完毕，设置正在加载数据的标志为 假
+            data_querying=false;
+            if (resp.errno=="0") {
+           total_page = resp.data.total_page;
+            // 如果当前页数为1，则清空原有数据
+                if (cur_page==1){ $(".list_con").html('')}
             // 显示数据
             for (var i=0;i<resp.data.news_list_dict.length;i++) {
                 var news = resp.data.news_list_dict[i]
